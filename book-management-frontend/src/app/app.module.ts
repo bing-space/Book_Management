d8eac14aa3acd92bc4544d3b8413e2a57395934f
Bing <bing334@gmail.com>
2025-07-01 18:19:19 -0400

    define routes and configures Router
    - define routes
    - configure router based on routes
    - define router outlet and renders the desired component based on route
    - setup router links

--- book-management-frontend/src/app/app.module.ts
+++ book-management-frontend/src/app/app.module.ts
@@ -4,13 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookService } from './services/book.service';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'books', component: BookListComponent},
+  {path: 'authors', component: AuthorListComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    DashboardComponent,
+    BookListComponent,
+    AuthorListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
